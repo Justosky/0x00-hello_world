4d534e430ac66988e3df7e60cf324ad61a0fc270
justosky <justosky2011@yahoo.com>
2023-03-16 13:07:12 +0000

    A C program that prints on the screen

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-int main(void)
+int main(void) /*This function is the main function that houses 
+               all other functions. Without it Nothing works in C */
 
 {
 	puts("\"Programming is like building a multilingual puzzle");
