3b64a3a99581ccfbee9966a89ff5ee537c75b583
justosky <justosky2011@yahoo.com>
2023-03-16 22:39:42 +0000

    This C program writes to stderr when executed

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,7 @@
 *main - Entry point
 *This program prints "and that piece of art is useful"
 * - Dora Korpar, 2015-10-19,"
-*The program returns 0 when the program runs 
+*The program returns 0 when the program runs
 *successful without error
 *Return: Always 0 (SUCCESS)
 */
