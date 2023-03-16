3bf7047aeb2a48aaa249563f8d6a4ea5a6178c02
justosky <justosky2011@yahoo.com>
2023-03-16 13:53:36 +0000

    A C program that prints on the screen

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+/**
+*main - Entry point
+*printf - Prints a string of text ending with a newline.
+*return - Always SUCCESS
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
