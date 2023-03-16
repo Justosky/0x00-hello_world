35477e7608a1878017cb02da0d8b219d29ce1a7f
justosky <justosky2011@yahoo.com>
2023-03-16 13:34:50 +0000

    A C program that prints on the screen

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
-int main(void)/* The main function */
+/**
+*main function takes no argument.
+*puts function is within main function.
+*puts function prints a string of text
+*main returns success (0) if the program runs successfully
+ */
+
+int main(void)
 
 {
 	puts("\"Programming is like building a multilingual puzzle");
