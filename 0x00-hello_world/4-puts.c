d506e61672a478938c3f31109c0f1c59e53c0a77
justosky <justosky2011@yahoo.com>
2023-03-16 13:23:01 +0000

    A C program that prints on the screen

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
-int main(void)/* This is the main function*/
-
+int main(void)
+/* This is the main function*/
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
