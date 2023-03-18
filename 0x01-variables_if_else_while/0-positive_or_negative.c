3c5790445416a7e701ed3efa8869faead3aa2637
justosky <justosky2011@yahoo.com>
2023-03-18 14:00:47 +0000

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -19,7 +19,8 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	if(n == 0)
-		{printf ("%i is zero\n",n);}
+		{printf
+		("%i is zero\n",n);}
 
         else if (n > 0)
 		{printf ("%i is positive\n",n);}
