89affa9c5291885e5461608986c497805eb6858c
justosky <justosky2011@yahoo.com>
2023-03-18 23:57:52 +0000

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+*main - Entry
+*Return: Always 0 (SUCCESS)
+*/
+
+int main(void)
+{
+	int num_of_base_sikstin;
+
+	for (num_of_base_sikstin = 48; num_of_base_sikstin < 58;
+	num_of_base_sikstin++)
+	{putchar(num_of_base_sikstin); }
+
+	for (num_of_base_sikstin = 97; num_of_base_sikstin < 122;
+	 num_of_base_sikstin++)
+	{putchar(num_of_base_sikstin); }
+
+	putchar('\n');
+
+	return (0);
+}
