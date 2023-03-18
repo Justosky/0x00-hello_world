819a2850947867157d7065c4c0dba3b09fe7569c
justosky <justosky2011@yahoo.com>
2023-03-18 21:04:05 +0000

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+/**
+*main - Entry point
+*Return: Always 0 (SUCCESS)
+*/
+
+int main(void)
+{
+	char lower_and_upper_case_letters[56]="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+	int _lower_and_upper_case_letters;
+
+	for (_lower_and_upper_case_letters = 0; _lower_and_upper_case_letters <= 52; _lower_and_upper_case_letters++)
+
+	{ putchar(lower_and_upper_case_letters[_lower_and_upper_case_letters]) ;}
+
+	return (0);
+}
