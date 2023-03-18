ecb97205f89fe61e938cee5c051fe2f914778381
justosky <justosky2011@yahoo.com>
2023-03-18 23:27:18 +0000

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+*main - Entry point
+*Return: Always 0 (SUCCESS)
+*/
+
+int main(void)
+{
+	char lower_case_alph_in_rev;
+
+	for (lower_case_alph_in_rev = 'z'; lower_case_alph_in_rev >= 'a';
+	lower_case_alph_in_rev--)
+	{putchar(lower_case_alph_in_rev); }
+
+	return (0);
+}
