bf62b6d30a9ce3100b0c8e31eba097bda4cb26a1
justosky <justosky2011@yahoo.com>
2023-03-18 22:10:01 +0000

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+*main - Entry point
+*Return: Always 0 (SUCCESS)
+*/
+
+int main(void)
+{
+	int single_digits_of_base_ten;
+
+	for (single_digits_of_base_ten = 0; single_digits_of_base_ten < 10;
+	single_digits_of_base_ten++)
+	{printf("%i", single_digits_of_base_ten); }
+
+	return (0);
+}
