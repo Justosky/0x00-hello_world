dfbaa785f3b444a76ad14f309526eb7a8aba97bf
justosky <justosky2011@yahoo.com>
2023-03-19 09:03:24 +0000

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,28 +11,22 @@ int first_comb_of_three_digits, second_comb_of_three_digits,
 	 third_comb_of_three_digits;
 
 	for (first_comb_of_three_digits = 48; first_comb_of_three_digits < 58;
-	 first_comb_of_three_digits++)
+	first_comb_of_three_digits++)
 	{
-
 	for (second_comb_of_three_digits = 49; second_comb_of_three_digits < 58;
-	 second_comb_of_three_digits++)
-		{
-
+	second_comb_of_three_digits++)
+	{
 	for (third_comb_of_three_digits = 50; third_comb_of_three_digits < 58;
-	 third_comb_of_three_digits++)
-			{
-
+	third_comb_of_three_digits++)
+	{
 	if (third_comb_of_three_digits > second_comb_of_three_digits
-		 && second_comb_of_three_digits > first_comb_of_three_digits)
-				{
-				putchar(first_comb_of_three_digits);
-				putchar(second_comb_of_three_digits);
-				putchar(third_comb_of_three_digits);
-
+		&& second_comb_of_three_digits > first_comb_of_three_digits)
+	{putchar(first_comb_of_three_digits);
+	putchar(second_comb_of_three_digits);
+	putchar(third_comb_of_three_digits);
 	if (first_comb_of_three_digits != 55 || second_comb_of_three_digits != 56)
-					{
-				putchar(',');
-				putchar(' ');
+	{ putchar(',');
+	putchar(' ');
 
 					}
 				}
