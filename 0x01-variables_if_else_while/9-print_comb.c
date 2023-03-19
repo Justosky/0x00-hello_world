9a5dd78342818601881878b98cf7f0c71ba8342b
justosky <justosky2011@yahoo.com>
2023-03-19 00:42:11 +0000

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,13 +12,13 @@ int main(void)
 
 	for (pos_comb_of_all_single_digits = 48;
 	pos_comb_of_all_single_digits < 58;
-	 pos_comb_of_all_single_digits++)
+	pos_comb_of_all_single_digits++)
 	{putchar(pos_comb_of_all_single_digits);
-	
+
 	if (pos_comb_of_all_single_digits != 57)
 		{putchar(',');
 		putchar(' '); }}
-	
+
 	putchar('\n');
 return (0);
 }
