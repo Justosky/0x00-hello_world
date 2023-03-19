d4c3d57bd06d945d40e995ff3f8c502dad7d4b6a
justosky <justosky2011@yahoo.com>
2023-03-19 07:42:13 +0000

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,24 +11,27 @@ int main(void)
 int first_comb_for_two_digits, second_comb_for_two_digits;
 
 
-for (first_comb_for_two_digits = 48; first_comb_for_two_digits < 57; first_comb_for_two_digits++)
+for (first_comb_for_two_digits = 48; first_comb_for_two_digits <= 56;
+	 first_comb_for_two_digits++)
 	{
-for (second_comb_for_two_digits = 49; second_comb_for_two_digits <= 57; second_comb_for_two_digits++)
-		{ if (first_comb_for_two_digits > second_comb_for_two_digits)
+for (second_comb_for_two_digits = 49;
+	second_comb_for_two_digits <= 57; second_comb_for_two_digits++)
+		{
+if (second_comb_for_two_digits > first_comb_for_two_digits)
 
-		     { 	if (first_comb_for_two_digits != 57 || second_comb_for_two_digits != 57)
-
-			{ putchar(first_comb_for_two_digits); 
+		     {  putchar(first_comb_for_two_digits);
 			putchar(second_comb_for_two_digits);
-				{putchar (','); 
-				putchar (' ');
+
+				if (first_comb_for_two_digits != 56 || second_comb_for_two_digits != 57)
+			{	putchar(',');
+				putchar(' ');
 
 				}
 			}
 		}
 
 	}
-}
+
 putchar('\n');
 return (0);
 
