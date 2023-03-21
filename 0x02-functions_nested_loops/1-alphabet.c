b131bce702352dd11cc6a4d1de7ef19f208f59fb
justosky <justosky2011@yahoo.com>
2023-03-21 15:40:00 +0000

    A C function that prints lowercase alphabets followed by a new line

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+
+/**
+*main - Entry point
+*Return: Always 0 (SUCCESS)
+*/
+
+int main(void)
+
+{
+	char all_lower_case_alphabets;
+
+	for (all_lower_case_alphabets = 'a';
+			all_lower_case_alphabets < 'z';
+				all_lower_case_alphabets++)
+
+
+	{	_putchar(all_lower_case_alphabets);
+		_putchar('\n');	}
+return (0);
+
+
+}
+}
