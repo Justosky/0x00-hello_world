20f0a9dc32f030034176fbbbfe67b7529c7b98fe
justosky <justosky2011@yahoo.com>
2023-03-22 09:22:53 +0000

    Checks for lowercase alphabets

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*_islower - Checks if a character is lowercase
+* and returns 1 if lowercase and 0 if uppercase
+*/
+
+int _islower(int c)
+{
+	char lower_case_alphabets;
+	if (lower_case_alphabets >= 'a' && lower_case_alphabets <= 'z')
+	{
+	return(1);
+	else (return (0))
+	}
+
+}
