288a16fa0d3d9227a9c39fe6bf7b0f2b823bdf11
justosky <justosky2011@yahoo.com>
2023-03-22 21:49:09 +0000

    Checks if number n is positive or negative

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* positive_or_negative - tests function that prints if integer
+* is positive or negative
+*@i: The number to be checked if positive or negative
+* Return: 0
+*/
+
+void positive_or_negative(int i)
+{
+	int i;
+
+	i = 98;
+	positive_or_negative(i);
+
+	return (0);
+}
