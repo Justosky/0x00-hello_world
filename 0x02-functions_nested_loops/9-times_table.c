10a7f0bfaa1bb14b86d75cd5e0c2e106b7f163e9
justosky <justosky2011@yahoo.com>
2023-03-22 15:15:01 +0000

    Prints 9X table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+*times_table - Prints nine times table on the console window
+*Return: No return value for this function
+*/
+
+void times_table(void)
+
+{
+	int n;
+
+	int nine = 9;
+
+	int nine_times_table = nine * (n + 1);
+
+for (n = 0; n < 13; n++)
+	{
+	_putchar(nine_times_table);
+		}
+}
