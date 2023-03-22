411105c28200ca617e16fdb35ef25547e2bc1b91
justosky <justosky2011@yahoo.com>
2023-03-22 00:25:33 +0000

    Print all lowercase alphabets ten times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+*print_alphabet_x10 - Prints all lowercase alphabets in ten places.
+*/
+
+void print_alphabet_x10(void)
+{
+
+	char lowercase_alphabets;
+
+	int multiply_by_ten
+
+	while (multiply_by_ten = 0;
+		multiply_by_ten <= 9;
+		 multiply_by_ten++)
+	{
+	for (lowercase_alphabets = 'a'; lowercase_alphabets <= 'z';
+			lowercase_alphabets++)
+		{ _putchar(lowercase_alphabets); }
+	}
+	_putchar ('\n');
+}
