842caafed61e72e6bd5e418db88b805ce78d1f91
justosky <justosky2011@yahoo.com>
2023-03-22 22:17:10 +0000

    Compares three integers and returns the highest

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,26 +1,30 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
-* largest_number - prints the largest of 3 integers
-*@a: One among the third numbers to be checked
-*@b: second among the third numbers to be checked
-*@c: Third among the third numbers to be checked
-* Return: 0
-*/
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
+ */
 
 int largest_number(int a, int b, int c)
 {
-	int a, b, c;
 	int largest;
 
-	a = 972;
-	b = -98;
-	c = 0;
+	if (a > b && b > c)
+	{
+	largest = a;
+	}
+	else if (b > a && a > c)
+	{
+	largest = b;
+	}
+	else
+	{
+	largest = c;
+	}
 
-	int largest = largest_number(a, b, c);
-
-	printf("%d is the largest number\n", largest);
-
-	return (0);
+	return (largest);
 }
+
