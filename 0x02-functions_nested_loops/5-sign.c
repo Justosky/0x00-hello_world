0ae6ca18e086d683a0d212ea362ff4e618b5b4e0
justosky <justosky2011@yahoo.com>
2023-03-22 12:40:55 +0000

    Prints the sign of an integer value

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,10 @@
 
 /**
 *print_sign - Print the sign of a number
-Return: Always 0 (SUCCESS)
+*@n: n is the number to be checked. If n is a positve number we print
+* + and return 1, if it is a negative number we print - and return -1,
+*else we will print 0 and return 0
+*Return: Always 0 (SUCCESS)
 */
 
 int print_sign(int n)
@@ -10,16 +13,20 @@ int print_sign(int n)
 	if (n > 0)
 	{
 	_putchar('+');
-	_putchar('n')}
+	return (1);
+	}
 
 	else if (n < 0)
 	{
-	_putchar('-'); }
+	_putchar('-');
+	return (-1);	 }
 
-	else {
-	_putchar('0'); }
+	else
+	{
+	_putchar('0');
+	return (0);	 }
 
-return(0);
+return (0);
 
 
 }
