87e8fe467e58f41f56792e7f32385b7e588a98f0
justosky <justosky2011@yahoo.com>
2023-03-22 14:53:53 +0000

    Prints all hours and minutes in jack bauers day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,19 +12,18 @@ void jack_bauer(void)
 
 	int jack_bauer_minutes;
 
-	for (jack_bauer_hours = 0; jack_bauer_hours < 23;
+	for (jack_bauer_hours = 0; jack_bauer_hours < 24;
 		 jack_bauer_hours++)
 		{
 		for (jack_bauer_minutes = 0; jack_bauer_minutes < 60;
 			 jack_bauer_minutes++)
 			{
-			_putchar((jack_bauer_hours / 10) + '0' );
-			_putchar((jack_bauer_hours % 10) + '0' );
+			_putchar((jack_bauer_hours / 10) + '0');
+			_putchar((jack_bauer_hours % 10) + '0');
 			_putchar(':');
-			_putchar((jack_bauer_minutes / 10) + '0' );
-			_putchar((jack_bauer_minutes % 10) + '0' );
-
-
+			_putchar((jack_bauer_minutes / 10) + '0');
+			_putchar((jack_bauer_minutes % 10) + '0');
+			_putchar('\n');
 			}
 		}
 }
