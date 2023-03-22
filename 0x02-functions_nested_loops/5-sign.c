77b2116f2ab1e79bd93a82d3cebf5c1629d8b66b
justosky <justosky2011@yahoo.com>
2023-03-22 12:21:11 +0000

    Checks if a character is an alphabet

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+*print_sign - Print the sign of a number
+Return: Always 0 (SUCCESS)
+*/
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+	_putchar('+'); }
+
+	else if (n < 0)
+	{
+	_putchar('-'); }
+
+	else {
+	_putchar('0'); }
+
+return(0);
+
+
+}
