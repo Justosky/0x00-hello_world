e8f3d7b7078da92ef9dc5e2ac4fd7a10b09fe020
justosky <justosky2011@yahoo.com>
2023-03-22 16:17:30 +0000

    Prints natural numbers defined as n

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,12 +14,14 @@ void print_to_98(int n)
 if (n >= 98)
 	{
 	while (n > 98)
-	printf("%i, " n--);
+	printf("%i, ", n--);
+	printf("%i\n", n);
 	}
 	else
 	{
 	while (n < 98)
-	printf("%i, " n++);
+	printf("%i, ", n++);
+	printf("%i\n" n);
 	}
 
 }
