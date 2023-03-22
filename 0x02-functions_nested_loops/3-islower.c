515b85d3a51ec5aa66d80077dc7a0a40e46363e1
justosky <justosky2011@yahoo.com>
2023-03-22 09:49:21 +0000

    Checks for lowercase alphabets

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,15 +2,21 @@
 
 /**
 *_islower - Checks if a character is lowercase
+*@c: - This is the character that we are checking
 * and returns 1 if lowercase and 0 if uppercase
+*Return: Always 0 (success);
 */
 
-int _islower(int c)
+int _islower(int c) /* c - is a variable*/
 {
-	if (c >= 'a' && c <= 'z')
+	if (c >= 'a' && c <= 'z')/* c - is a variable */
 	{
-	return(1);}
-	else {return (0);}
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
 
 
 }
