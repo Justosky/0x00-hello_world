3adccdaa9a2adcb0ccf6f3b8807acc304d833044
justosky <justosky2011@yahoo.com>
2023-03-22 00:29:22 +0000

    Print all lowercase alphabets ten times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,15 +6,13 @@
 
 void print_alphabet_x10(void)
 {
-
 	char lowercase_alphabets;
 
-	int multiply_by_ten
+	int multiply_by_ten;
 
 	while (multiply_by_ten = 0;
 		multiply_by_ten <= 9;
-		 multiply_by_ten++)
-	{
+		 multiply_by_ten++){
 	for (lowercase_alphabets = 'a'; lowercase_alphabets <= 'z';
 			lowercase_alphabets++)
 		{ _putchar(lowercase_alphabets); }
