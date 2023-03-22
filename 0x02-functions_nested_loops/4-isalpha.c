c21b98dd0eae1f9e22dd5dc1914c4793c094df28
justosky <justosky2011@yahoo.com>
2023-03-22 11:32:53 +0000

    Checks if a character is an alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+*_isalpha - Checks for lower or uppercase letters and
+*@c: Checks if a character is an alphabet
+*return 1 if it exist else it returns 0
+*Return: Always 0 (SUCCESS)
+*/
+
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+	if (c >= 'a' && c <= 'z')
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+	}
+}
