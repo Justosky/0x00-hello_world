07a57b19ffbdd8d882b56c7b2610f9220fe7e93f
justosky <justosky2011@yahoo.com>
2023-03-22 14:41:24 +0000

    Prints all hours and minutes in jack bauers day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+*jack_bauer - prints all hours and minutes on jack bauer favorite day.
+*Return: Always 0 (SUCCESS)
+*/
+
+void jack_bauer(void)
+
+{
+	int jack_bauer_hours;
+
+	int jack_bauer_minutes;
+
+	for (jack_bauer_hours = 0; jack_bauer_hours < 23;
+		 jack_bauer_hours++)
+		{
+		for (jack_bauer_minutes = 0; jack_bauer_minutes < 60;
+			 jack_bauer_minutes++)
+			{
+			_putchar(jack_bauer_hours / 10);
+			_putchar(jack_bauer_hours % 10);
+			_putchar(':');
+			_putchar(jack_bauer_minutes / 10);
+			_putchar(jack_bauer_minutes % 10);
+
+
+			}
+		}
+			return (0);
+}
