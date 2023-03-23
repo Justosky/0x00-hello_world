47fb812f6b102387b5bee49413934ee591897900
justosky <justosky2011@yahoo.com>
2023-03-23 00:34:11 +0000

    Test that our function gives the correct output

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -3,8 +3,6 @@
 
 /**
 * main - tests function that prints if integer is positive or negative
-*@i: The integer value to be checked
-*positive_or_negative - Checks if  an integer is positive or negative
 * Return: 0
 */
 
