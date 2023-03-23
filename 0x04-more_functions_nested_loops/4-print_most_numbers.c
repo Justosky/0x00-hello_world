1c37aebc68e832d30b330d6a7686c407a965fa61
justosky <justosky2011@yahoo.com>
2023-03-23 22:15:10 +0000

    Prints natural numbers to 9 excluding 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,18 +1,19 @@
-#include "stdio.h"
+#include "main.h"
 
 /**
 *print_most_numbers - Prints numbers from 0 - 9 exluding 2 and 4.
 *Return: This function returns nothing
 */
+
 void print_most_numbers(void)
 {
 char num;
 
 	for (num = '0'; num <= '9'; num++)
-		{
-		for (!(num == '2')||(num == '4'))
-			{
-				_putchar(num);	}
+	{
+	if (!(num == '2') || (num == '4'))
+	{
+	_putchar(num);	}
 }
 _putchar('\n');
 }
