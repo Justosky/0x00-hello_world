a45afb1cab693d90fe9d937a55f0cec9277758dd
justosky <justosky2011@yahoo.com>
2023-03-23 21:35:21 +0000

    Prints the numbers 0 - 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*print_numbers - This function prints numbers from 0 - 9 
+*followed by a new line.
+*Return: returns nothing
+*/
+
+
+void print_numbers(void)
+{
+int a;
+
+for(a = 0; a <= 9; a++)
+	{_putchar(a);}
+
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -26,5 +26,4 @@ void print_triangle(int size);
 
 
 
-
 #endif
