1699a9c76241ca62dd98dc03e8fce63c5fc84465
justosky <justosky2011@yahoo.com>
2023-03-23 00:43:22 +0000

    Test that our function gives the correct output

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,6 @@
 #include "main.h"
 #include <stdio.h>
 
-
 /**
 * main - tests function that prints if integer is positive or negative
 * Return: 0
@@ -14,6 +13,6 @@ int main(void)
 
 	i = 0;
 	positive_or_negative(i);
-	printf("Good \n")
+	printf("Good \n");
 	return (0);
 }
