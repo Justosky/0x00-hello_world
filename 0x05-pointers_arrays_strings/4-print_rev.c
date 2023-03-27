8f9173e6f6e584657361c1c2a73ff2dc4f31e368
justosky <justosky2011@yahoo.com>
2023-03-27 17:40:26 +0000

    Prints the string s in reverse order

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -14,13 +14,12 @@ void print_rev(char *s)
 	int count = 0;
 
 	while (s[count] != '\0')
-	{
+	
 	count++;
 	
 	for (reverse_s = count -1; reverse_s >= 0; reverse_s--)
 	{
 	_putchar(s[reverse_s]);
-	}
 	}
 
 _putchar('\n');
