824a31cf4c06687a0c8269db2062bc050cf91c2a
justosky <justosky2011@yahoo.com>
2023-03-27 10:53:31 +0000

    Updates a variable value using pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *reset_to_98 - Resets the previous value to 98
+ *@*n: The pointer which will reset the value to 98
+ *Return: Returns void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
