9d78c35562f324774b0ba5067b63445d0a824301
justosky <justosky2011@yahoo.com>
2023-04-03 17:56:23 +0000

    Prints cheeseboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- *void print_chessboard - A function that prints the chessboard.
+ *print_chessboard - A function that prints the chessboard.
  *@a: A character pointer array.
  *Return: This function returns void.
  */
@@ -12,14 +12,14 @@ void print_chessboard(char (*a)[8])
 	int count_for_array_a_row, count_for_array_a_collumn;
 
 	for (count_for_array_a_row = 0; count_for_array_a_row < 8;
-		count_for_array_a_row++)
+	count_for_array_a_row++)
 	{
-		for (count_for_array_a_collumn = 0;
-			count_for_array_a_collumn < 8;
-			count_for_array_a_collumn++)
-		{
+	for (count_for_array_a_collumn = 0;
+	count_for_array_a_collumn < 8;
+	count_for_array_a_collumn++)
+	{
 	printf("%c", a[count_for_array_a_row][count_for_array_a_collumn]);
-		}
+	}
 		printf("\n");
 	}
 
