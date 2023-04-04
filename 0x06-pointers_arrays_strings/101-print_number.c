c5cbd1c5e15507ecdc9f45970a66e3fadb767bc1
justosky <justosky2011@yahoo.com>
2023-04-04 22:02:53 +0000

    Prints a number

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,16 +8,19 @@
 
 void print_number(int n)
 {
+	unsigned int n_one;
 
 	if (n < 0)
 	{
 		_putchar('-');
-		n *= -1;
+		n = -n;
 	}
 
-	if (n / 10)
+	n_one = n;
+
+	if (n_one / 10)
 	{
-	print_number(n / 10);
+	print_number(n_one / 10);
 	}
-	_putchar(n % 10 + '0');
+	_putchar(n_one % 10 + '0');
 }
