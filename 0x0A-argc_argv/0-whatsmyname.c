e54ac5bcc465fa3f0efb4a37756849681bacf567
justosky <justosky2011@yahoo.com>
2023-04-07 19:10:36 +0000

    Prints it's name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 /**
- *
- *
- * */
+ *main - Entry point
+ *@argc: Our arguement count.
+ *@argv: Our arguement vector.
+ *Return: Returns an Integer.
+ */
 
 int main(int argc, char *argv[])
 {
-	printf("%s\n",argv[0]);
+	printf("%s\n", argv[0]);
 	return (0);
-
 }
