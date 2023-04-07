b559b45b1d3801a3e5d7b5ab532c6f8ce81b09d9
justosky <justosky2011@yahoo.com>
2023-04-07 20:05:15 +0000

    Prints it's name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,34 +1,17 @@
-#include "main.h"
-#include <unistd.h>
-
-/**
- *_putchar - Prints a character to the console.
- *@c: The character to be printed.
- */
-
-void _putchar(char c)
-{
-
-	write(STDOUT_FILENO, &c, 1);
-
-}
+#include <stdio.h>
 
 /**
  *main - Entry point
- *@argc: Our arguement count.
- *@argv: Our arguement vector.
- *Return: Returns an Integer.
+ *@argc: Arguement counter.
+ *@argv: Arguement vector.
+ *Return: Returns an interger.
  */
 
-int main(int argc __attribute__ ((unused)), char *argv[])
+int main(int argc, char *argv[])
 {
+	(void) argc;
 
-	char *name = argv[0];
+	printf("%s\n", argv[0]);
 
-	while (*name != '\0')
-	{
-		_putchar(*name++);
-	}
-	_putchar ('\n');
 	return (0);
 }
