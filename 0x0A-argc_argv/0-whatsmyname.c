c96549025d4e94bf3ac94afbc4560fc65d8d4c05
justosky <justosky2011@yahoo.com>
2023-04-07 19:49:55 +0000

    Prints it's name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,17 @@
-#include <stdio.h>
+#include "main.h"
+#include <unistd.h>
+
+/**
+ *_putchar - Prints a character to the console.
+ *@c: The character to be printed.
+ */
+
+void _putchar(char c)
+{
+
+	write(STDOUT_FILENO, &c, 1);
+
+}
 
 /**
  *main - Entry point
@@ -7,8 +20,15 @@
  *Return: Returns an Integer.
  */
 
-int main(int argc, char *argv[])
+int main(int argc __attribute__ ((unused)), char *argv[])
 {
-	printf("%s\n", argv[0]);
+
+	char *name = argv[0];
+
+	while (*name != '\0')
+	{
+		_putchar(*name++);
+	}
+	_putchar ('\n');
 	return (0);
 }
