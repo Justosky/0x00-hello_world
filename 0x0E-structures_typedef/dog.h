eb229cde44f1f106992b15f84be3a14c621f2c8d
justosky <justosky2011@yahoo.com>
2023-04-18 13:35:00 +0000

    Added typedef to struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,7 +8,7 @@
 *@owner: The owner of the dog.
 */
 
-typedef struct dog dog_t
+struct dog
 {
 	char *name;
 	float age;
@@ -23,4 +23,5 @@ typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 
 void free_dog(dog_t *d);
+typedef struct dog dog_t;
 #endif
