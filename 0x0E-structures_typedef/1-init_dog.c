f6e73fe2b949e3dc917ea2d408aabb00f8298d14
justosky <justosky2011@yahoo.com>
2023-04-18 12:17:42 +0000

    Initializes a dog variable

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -6,13 +6,15 @@
 *@name: The name of the dog.
 *@age: Age of the dog.
 *@owner: The owner of the dog.
+*@d: A pointer.
 */
-void init_dog(struct dog *d, char *name, float age, char *owner);
 
- void init_dog(struct dog *d, char *name, float age, char *owner)
-{	
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
 	d->name = name;
+
 	d->age = age;
+
 	d->owner = owner;
-	
+
 }
