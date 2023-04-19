233caac6522ffb4cb5f82060b3b4bfcd9798a23f
justosky <justosky2011@yahoo.com>
2023-04-19 13:10:38 +0000

    Prints an index

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,30 @@
+#include "function_pointers.h"
+
+/**
+  * int_index - The index 
+  * @array: An array
+  * @size: Size of the array
+  * @cmp: Cmp
+  * Return: Returns an integer
+  */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int b;
+
+	if (size > 0)
+	{
+		if (array != NULL && cmp != NULL)
+		{
+			for (b = 0; b < size; b++)
+			{
+				if (cmp(array[b]))
+					return (b);
+
+			}
+		}
+	}
+
+	return (-1);
+}
+
