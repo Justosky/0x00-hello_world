39766427b8f521fe736e9ac0b4187a24c3fb8f77
justosky <justosky2011@yahoo.com>
2023-04-27 15:52:05 +0100

    Contains functions prototypes

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,26 @@
 #ifndef lists_h
 #define lists_h
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+*struct list_s - A structure with name list_s and a data type of
+*struct list_s. It has three members:
+* a char pointer *str, Unsigned int len and and
+*and a pointer next which points to struct list_s
+*@str: A pointer and a member of the structure.
+*@len: A variable of unsigned int datatype and a member of the structure
+*@next: A pointer which points to the structure which it is a member of.
+*It will be used to transverse the different nodes created with the structure.
+*/
+
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
