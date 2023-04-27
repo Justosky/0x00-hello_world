650a475829cd43110bf4c851acb01f4f08fc6fa8
justosky <justosky2011@yahoo.com>
2023-04-27 18:35:21 +0100

    A special function that executes before the main function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+*print_message - A special function that is executed before the main function.
+*/
+
+void print_message(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
