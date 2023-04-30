68c630b8141802292b4461c1b75a4c462721742c
justosky <justosky2011@yahoo.com>
2023-04-30 14:48:05 +0000

    Adds a newnode at the start of a list

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -24,11 +24,10 @@ listint_t *add_nodeint(listint_t **head, const int n)
 		}
 		else if (*head != NULL)
 		{
-		newnode->n = n;
 		newnode->next = *head;
 		*head = newnode;
 		}
 	}
+	newnode-n = n;
 	return (newnode);
-	printf("\n");
 }
