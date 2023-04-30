9e109e46da4fd30e55dff0ea71b23f166f986007
justosky <justosky2011@yahoo.com>
2023-04-30 13:44:14 +0000

    Returns the total number of elements in a list

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,5 +1,13 @@
 #include "lists.h"
 
+/**
+*listint_len - A function that prints the total elements in
+*a listint_t list.
+*@h: A pointer which points to the first element of the list.
+*Return: Returns an interger which is the total number of the
+*the elements within the list
+*/
+
 size_t listint_len(const listint_t *h)
 {
 
@@ -19,7 +27,7 @@ size_t listint_len(const listint_t *h)
 
 			count++;
 			head = head->next;
-			
+
 		}
 	}
 
