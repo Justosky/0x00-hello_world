42a32baf559b687a33af4ecbbcb222d237d70dea
justosky <justosky2011@yahoo.com>
2023-04-30 13:37:59 +0000

    Prints all elements of a list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -13,12 +13,11 @@ size_t print_listint(const listint_t *h)
 	size_t count = 0;
 	const listint_t *head = h;
 
-	
 	if (head == NULL)
 	{
 		return (count);
 	}
-	
+
 	else
 	{
 	for (; head != NULL ;)
