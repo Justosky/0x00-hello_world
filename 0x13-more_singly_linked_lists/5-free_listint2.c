ae73abc341841dfbc46a0099625cf67f2012b005
justosky <justosky2011@yahoo.com>
2023-04-30 20:36:50 +0000

    Frees a listint_s list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+*free_listint2 - Frees a listint_t list
+*@head: A double pointer that points to a pointer that points to the
+*the first element of the list.
+*Returns: This function returns nothing or void.
+*/
+
+void free_listint2(listint_t **head)
+{
+	listint_t *free_list = *head;
+
+	for (; *head != NULL ;)
+	{
+		free_list = *head;
+		*head = (*head)->next;
+		free(free_list);
+	}
+		*head = NULL;
+}
