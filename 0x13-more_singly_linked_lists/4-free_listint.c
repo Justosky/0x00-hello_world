31429f540b011d3c9734c12e19bf99142b9c5871
justosky <justosky2011@yahoo.com>
2023-04-30 19:14:59 +0000

    Frees a listint_s list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+*free_listint - A function that frees listint_t list
+*@head: A pointer that points to the first node of the list
+*Return: Returns nothing.
+*/
+
+void free_listint(listint_t *head)
+{
+	listint_t *free_list;
+
+	for (; head != NULL ;)
+	{
+		free_list = head;
+		head = head->next;
+		free(free_list);
+	}
+
+}
+
