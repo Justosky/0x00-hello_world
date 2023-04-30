5be21d2d385995376ac96c1602898e6fd417d339
justosky <justosky2011@yahoo.com>
2023-04-30 14:42:16 +0000

    Adds a newnode at the start of a list

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -10,18 +10,25 @@
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *newnode = malloc(sizeof(listint_t));
+
 	newnode->n = n;
 
-
-	if (*head == NULL)
+	if (newnode == NULL)
 	{
+	return (NULL);
+	}
+	else
+	{
+		if (*head == NULL)
+		{
 		*head = newnode;
 		newnode->next = NULL;
-	}
-	else if (*head != NULL)
-	{
+		}
+		else if (*head != NULL)
+		{
 		newnode->next = *head;
 		*head = newnode;
+		}
 	}
 	return (newnode);
 	printf("\n");
