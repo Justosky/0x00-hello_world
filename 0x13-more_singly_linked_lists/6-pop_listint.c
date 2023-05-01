71129c30483d98990901aeb6bd83deed832a3503
justosky <justosky2011@yahoo.com>
2023-05-01 14:16:39 +0000

    Frees the first node of a list

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,5 +1,16 @@
 #include "lists.h"
 
+/**
+*pop_listint - A function that deletes the first node of a list
+*and sets it's nth member to data and returns data. If the list is empty
+*it returns 0.
+*@head: A double pointer which points to another pointer that points
+*to the first node of the listint_t list.
+*Return: This function returns an interger which is
+*the value of the nth memeber *of the list. It returns 0
+*when the list is empty.
+*/
+
 int pop_listint(listint_t **head)
 {
 	listint_t *temp;
