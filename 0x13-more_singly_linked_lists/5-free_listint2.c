084018fc1c05d034b60cabf87093be154e0228e0
justosky <justosky2011@yahoo.com>
2023-05-01 11:08:53 +0100

    Frees a listint_t list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,7 +10,7 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *free_list = *head;
-	if (head == NULL)
+	if (*head == NULL)
 	return;
 	for (; (*head) != NULL ;)
 	{
@@ -18,5 +18,4 @@ void free_listint2(listint_t **head)
 		free(*head);
 		*head = free_list;
 	}
-
 }
