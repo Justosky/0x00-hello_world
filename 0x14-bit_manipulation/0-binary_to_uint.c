3e02fea866b7dcc04051948f2622a14f1e832aa4
justosky <justosky2011@yahoo.com>
2023-05-04 15:13:08 +0000

    Returns a decimal value for a given binary number

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,26 +11,24 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int result = 0;
-	int count;
+	int count = 0;
 
-	if (*b == '\0')
+	if (*(b) == '\0')
+
+	return (0);
+
+	for ( count = 0; *(b + count) != '\0'; count++) {
+        if (*(b + count) == '0' || *(b + count) == '1')
 	{
-		return (0);
+	result = result << 1;
+	result = result +  *(b + count) - '0'; 
 	}
-	else if (*b)
+	else
 	{
-		for (count = 0; *(b + count); count++)
-		{
-			if (*(b + count) == '0' || *(b + count) == '1')
-			{
-				result = result << 1;
-				result = result + (*(b + count) - '0');
-			}
-			else
-			{
-				return (0);
-			}
-		}
+            return (0);
+        }
 	}
-return (result);
+	return (result);
 }
+
+	
