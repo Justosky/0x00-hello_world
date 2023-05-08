7dfe8fce41fe23c2e75cb19e8b8baafe4ac0c099
justosky <justosky2011@yahoo.com>
2023-05-08 17:50:44 +0000

    Reads a file and print it's content on STDOUT

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,48 +1,38 @@
 #include "main.h"
 
+/**
+* read_textfile - A function that reads a text file and prints it's
+*content to the standard output.
+*@filename: A pointer which contains the strings to be printed.
+*@letters: The number of character to print.
+*Return: Returns an the total number of btytes written to standard output.
+*/
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int file_descriptor;
-	ssize_t bytes_read, bytes_written;
-	char *BUFFER = malloc(sizeof(char) * letters);
+	ssize_t file_descriptor, bytes_read, bytes_written;
+	char *buffer;
 
-	if ((BUFFER == NULL) || (filename == NULL))
+	if (filename == NULL)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+
+	file_descriptor = open(filename, O_RDONLY);
+	bytes_read = read(o, buffer, letters);
+	bytes_written = write(STDOUT_FILENO, buffer, r);
+
+	if (file_descriptor == -1 || bytes_read == -1 || bytes_written == -1 ||
+		bytes_read != bytes_written)
 	{
+		close(file_descriptor);
+		free(buffer);
 		return (0);
 	}
-	else
-	{
-		file_descriptor = open(filename, O_RDONLY);
 
-		if (file_descriptor == -1)
-		{
-			return (0);
-		}
-		else
-		{
-			bytes_read = read(file_descriptor, BUFFER, letters);
-			if (bytes_read == -1)
-			{
-				close(file_descriptor);
-				free(BUFFER);
-				return (0);
-			}
-			else
-			{
-				bytes_written = write(STDOUT_FILENO, BUFFER, bytes_read);
-				if (bytes_read != bytes_written)
-				{
-					close(file_descriptor);
-					free(BUFFER);
-					return (0);
-				}
-				else
-				{
-					close(file_descriptor);
-					free(BUFFER);
-					return (bytes_written);
-				}
-			}
-		}
-	}
+	free(buffer);
+	close(file_descriptor);
+	return (bytes_written);
 }
