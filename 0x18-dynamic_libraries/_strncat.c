a07528f29588ebc28d0d2bd486835df4202ffb09
justosky <justosky2011@yahoo.com>
2023-06-19 12:03:25 +0100

    _strncat.c

---
+++ 0x18-dynamic_libraries/_strncat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/*
+ *_strncat - A function that concatenates two string*
+ *@dest: The destination string
+ *@src: The source string
+ *@n: The number of character from src that will be concatenated to dest
+ *Returns: returns an interger.*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+
+}
