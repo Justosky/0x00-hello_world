c54fbf8244ea479fd66e52b2d2fe43b0475fe9f9
justosky <justosky2011@yahoo.com>
2023-06-19 11:50:45 +0100

    _memcpy.c

---
+++ 0x18-dynamic_libraries/_memcpy.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/*
+ *_memcpy - A function that copies src to dest*
+ *@dest: desination 
+ *@src: source
+ *@n: the number of character from src that will be copied to dest.*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+
+}
