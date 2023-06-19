ec57499888f21eaec62eb433c859fcbc9743f449
justosky <justosky2011@yahoo.com>
2023-06-19 12:55:59 +0100

    _strlen.c

---
+++ 0x18-dynamic_libraries/_strlen.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/*
+ *_strlen - A function that computes the length of a string
+ *@s: the string whose length will be computed
+ *Return: Returns an interger.*/
+
+int _strlen(char *s)
+{
+
+}
