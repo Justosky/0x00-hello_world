28d5e3a12d693b72c64e3969e1b30b6a1a992802
justosky <justosky2011@yahoo.com>
2023-06-19 12:15:55 +0100

    _islower.c

---
+++ 0x18-dynamic_libraries/_islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/*
+ *_islower - A function that checks if qa character is lowercase*
+ *@c: the character to be checked if it is lower alphabet.
+ *Return: Returns an interger.*/
+
+int _islower(int c)
+{
+
+}
