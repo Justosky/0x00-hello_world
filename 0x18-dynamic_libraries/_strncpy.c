ccdc6c7ac7e2688b5f8a4543a7b160cd9a1fade6
justosky <justosky2011@yahoo.com>
2023-06-19 12:30:26 +0100

    _strncpy.c

---
+++ 0x18-dynamic_libraries/_strncpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/*
+ *_strncpy - A function that copy the string src to dest
+ *@dest: destination
+ *@src: source
+ *@n: The number of characters from src that will be copied to dest.
+ *Return: A function that returns a character pointer*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+
+}
