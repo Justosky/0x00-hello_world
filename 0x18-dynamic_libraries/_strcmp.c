4e6960f432913cd17c75e30d0396170cb8f2c723
justosky <justosky2011@yahoo.com>
2023-06-19 11:58:42 +0100

    _strcmp.c

---
+++ 0x18-dynamic_libraries/_strcmp.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/*
+ *_strcmp - A function that compares two strings *s1 and *s2
+ *@s1: the first string
+ *@s2: the second string
+ *Return: returns an interger*/
+
+int _strcmp(char *s1, char *s2)
+{
+
+}
