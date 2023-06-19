930cd8357de63824513f93510af23f6c7cbe9336
justosky <justosky2011@yahoo.com>
2023-06-19 12:36:16 +0100

    _strstr.c

---
+++ 0x18-dynamic_libraries/_strstr.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/*
+ *_strstr - A function that manipulates strings
+ *@haystack: first string
+ *@needle: second string
+ *Return: Returns a a character pointer*/
+
+char *_strstr(char *haystack, char *needle)
+{
+
+}
