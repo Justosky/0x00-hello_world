c9a55447b77e0c71d9dc7f7dab8e89abcc1d6ef5
justosky <justosky2011@yahoo.com>
2023-06-19 13:18:08 +0100

    _strcpy.c

---
+++ 0x18-dynamic_libraries/_strcpy.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/*
+ *_strcpy - A function that copies src to dest
+ *@dest: destination
+ *@src: source
+ *Return: Returns an interger*/
+
+char *_strcpy(char *dest, char *src)
+{
+
+}
