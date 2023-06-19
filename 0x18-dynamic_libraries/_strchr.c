1db921e181151981e1319480ec5063366ae6b597
justosky <justosky2011@yahoo.com>
2023-06-19 12:53:13 +0100

    _strchr.c

---
+++ 0x18-dynamic_libraries/_strchr.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/*
+ *_strchr -  A function that manipulates a string a character
+ *@c: the character to be manipulated
+ *@s: The string to be manipulated
+ *Return: Returns an interger*/
+
+char *_strchr(char *s, char c)
+{
+
+}
