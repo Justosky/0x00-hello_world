b6de8e52abddaeb4065bd5653b0355e2e5d174da
justosky <justosky2011@yahoo.com>
2023-06-19 12:59:11 +0100

    _strpbrk.c

---
+++ 0x18-dynamic_libraries/_strpbrk.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/*
+ *_strpbrk - A function that plays with string
+ *@s: first string
+ *@accept: second string
+ *Return: Returns an interger*/
+
+char *_strpbrk(char *s, char *accept)
+{
+
+}
