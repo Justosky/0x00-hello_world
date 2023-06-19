d5c6df0f15c7eebc74f3fbe319347ea7c529dabd
justosky <justosky2011@yahoo.com>
2023-06-19 12:11:46 +0100

    _abs.c

---
+++ 0x18-dynamic_libraries/_abs.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/*
+ *_abs - A function that computes the absolute value of an interger n
+ *@n: The interger whose absolute value willl be computed
+ *Return: Returns an interger*/
+
+int _abs(int n)
+{
+
+}
