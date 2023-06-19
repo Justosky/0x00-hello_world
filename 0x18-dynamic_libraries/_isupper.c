7fac5c43de26d524141e8ccaa75bc65391b89430
justosky <justosky2011@yahoo.com>
2023-06-19 12:45:37 +0100

    _isupper.c

---
+++ 0x18-dynamic_libraries/_isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/*
+ *_isupper - A function that checks if a character is uppercase
+ *@c: The character to be checked
+ *Return: Returns an interger*/
+
+int _isupper(int c)
+{
+
+}
