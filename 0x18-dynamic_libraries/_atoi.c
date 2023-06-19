98d0fd05992865b1b697eca23010ac5dd28703b5
justosky <justosky2011@yahoo.com>
2023-06-19 13:02:47 +0100

    _atoi.c

---
+++ 0x18-dynamic_libraries/_atoi.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/*
+ *_atoi - A function that converts a string to intergers
+ *@s: the string to be converted to intergers
+ *Return: Returns an interger.*/
+
+int _atoi(char *s)
+{
+
+}
