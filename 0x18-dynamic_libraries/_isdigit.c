414d5f41706f2b43b0729d93ea81e0012c16fb86
justosky <justosky2011@yahoo.com>
2023-06-19 11:43:54 +0100

    _isdigit.c

---
+++ 0x18-dynamic_libraries/_isdigit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/*
+ *_isdigit - A function that checks if a character is digit or not
+ *@c: The character to be checked
+ *Return: Returns an interger
+ */
+
+int _isdigit(int c)
+{
+
+}
