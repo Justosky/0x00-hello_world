0a14e99dcee1a8adfa2667b9b65ea531082eccf6
justosky <justosky2011@yahoo.com>
2023-06-19 12:41:12 +0100

    _isalpha.c

---
+++ 0x18-dynamic_libraries/_isalpha.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/*
+ *_isalpha - A function that checks if a character is uppercase.
+ *@c: The character to be checked if uppercase.
+ *Return: Returns an interger.*/
+
+int _isalpha(int c)
+{
+
+}
