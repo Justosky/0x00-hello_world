3c77445cf1356d7842e14078bc676f0d2cf209c2
justosky <justosky2011@yahoo.com>
2023-06-19 12:48:58 +0100

    _putchar.c

---
+++ 0x18-dynamic_libraries/_putchar.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/*
+ *_putchar - A function that prints a character
+ *@c: the character that is to be printed
+ *Return: Returns an interger.*/
+
+int _putchar(char c)
+{
+
+}
