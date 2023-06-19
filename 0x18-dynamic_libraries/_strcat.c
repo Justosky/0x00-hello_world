ab2d7bf14a920accc1d6c867751c560900e02389
justosky <justosky2011@yahoo.com>
2023-06-19 12:23:40 +0100

    _strcat.c

---
+++ 0x18-dynamic_libraries/_strcat.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/*
+ *_strcat - A function that concatenates strings
+ *@dest: destination
+ *@src: source
+ *Return: Returns a character pointer*/
+
+char *_strcat(char *dest, char *src)
+{
+
+}
