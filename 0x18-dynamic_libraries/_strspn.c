6fc5cedd1edb3a956ac44053c267e0d569a8ba12
justosky <justosky2011@yahoo.com>
2023-06-19 12:08:13 +0100

    _strspn.c

---
+++ 0x18-dynamic_libraries/_strspn.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/*
+ *_strspn - A function that manipulates strings
+ *@s: first string
+ *@accept: second string
+ *Return: Returns an unsigned int*/
+
+unsigned int _strspn(char *s, char *accept)
+{
+
+}
